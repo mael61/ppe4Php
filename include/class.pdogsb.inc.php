3987baaf49a15a1ae112b6d6412bb1244d6cdd74
Lothaire Rouger <lrouger@sio-coliniere44.local>
2017-05-19 08:18:52 +0200

    retour en arrière precedent commit

--- include/class.pdogsb.inc.php
+++ include/class.pdogsb.inc.php
@@ -17,9 +17,9 @@
 
 class PdoGsb{   		
       	private static $serveur='mysql:host=localhost';
-      	private static $bdd='dbname=Piroma_PHP';   		
+      	private static $bdd='dbname=piroma_php';   		
       	private static $user='root' ;  
-      	private static $mdp='root' ;
+      	private static $mdp='' ;
 		private static $monPdo;
 		private static $monPdoGsb=null;
 /**
